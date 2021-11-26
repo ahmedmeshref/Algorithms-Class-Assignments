77cbaaa43bc0c39a8c78e02ce1f6fc8998b7f51b
ahmedmeshref <a.meshref@alustudent.com>
2021-11-26 20:43:23 +0200

    Implement more optimal solution for MST

--- assignment_2/min_spanning_tree.py
+++ assignment_2/min_spanning_tree.py
@@ -3,28 +3,35 @@ import sys
 
 
 def costMinSpanningTree(graph, startingVertex):
+    currVertex = startingVertex - 1
     # Number of vertices in graph
     V = len(graph)
-    # Total number of seen edges
-    no_edge = 0
+    minSpanningTree = []
+    validEdges = []
     # Seen contains indices of all seen vertices
-    seen = {startingVertex - 1}
-    total_cost = 0
-    while no_edge < V - 1:
-        minimum_edge_cost = sys.maxsize
-        fromV, toV = 0, 0
-        for vertex1 in seen:
-            for vertex2 in range(V):
-                # If vertex2 not seen before and there is an edge between vertex1 and vertex2
-                if (vertex2 not in seen) and graph[vertex1][vertex2]:
-                    if minimum_edge_cost > graph[vertex1][vertex2]:
-                        minimum_edge_cost = graph[vertex1][vertex2]
-                        fromV, toV = vertex1, vertex2
-        print(str(fromV + 1) + " -> " + str(toV + 1) + " : " + str(graph[fromV][toV]))
-        seen.add(toV)
-        no_edge += 1
-        total_cost += minimum_edge_cost
-    return total_cost
+    seen = set()
+    while len(minSpanningTree) < V - 1:
+        seen.add(currVertex)
+        minimumCostEdge = [0, 0, sys.maxsize]
+        # Get all valid vertices from the currVertex
+        for vertex2 in range(V):
+            # if there is an edge
+            if graph[currVertex][vertex2] != 0 and vertex2 not in seen:
+                validEdges.append([currVertex, vertex2, graph[currVertex][vertex2]])
+
+        # Calc edge with minimum cost in all valid edges
+        for edge in validEdges:
+            if edge[2] < minimumCostEdge[2]:
+                minimumCostEdge = edge
+
+        # Add edge to minSpanningTree
+        minSpanningTree.append([minimumCostEdge[0] + 1, minimumCostEdge[1] + 1, minimumCostEdge[2]])
+        # Remove edge from list of all valid edges
+        validEdges.remove(minimumCostEdge)
+        # Update curr vertex
+        currVertex = minimumCostEdge[1]
+
+    return minSpanningTree
 
 
 # g = [[0, 19, 5, 0, 0],
@@ -33,10 +40,10 @@ def costMinSpanningTree(graph, startingVertex):
 #      [0, 9, 1, 0, 1],
 #      [0, 2, 6, 1, 0]]
 g1 = [[0, 28, 0, 0, 0, 10, 0],
-     [28, 0, 16, 0, 0, 0, 14],
-     [0, 16, 0, 12, 0, 0, 0],
-     [0, 0, 12, 0, 22, 0, 18],
-     [0, 0, 0, 22, 0, 25, 24],
-     [10, 0, 0, 0, 25, 0, 0],
-     [0, 14, 0, 18, 24, 0, 0]]
+      [28, 0, 16, 0, 0, 0, 14],
+      [0, 16, 0, 12, 0, 0, 0],
+      [0, 0, 12, 0, 22, 0, 18],
+      [0, 0, 0, 22, 0, 25, 24],
+      [10, 0, 0, 0, 25, 0, 0],
+      [0, 14, 0, 18, 24, 0, 0]]
 print(costMinSpanningTree(g1, 6))
